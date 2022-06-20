233cfe0b43137b2fa352eba40d0f87639d46d9a1
Dorian Chibuc <devel@ywemay.com>
2022-06-20 14:38:46 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+exports.ADMIN = 'admin';
+exports.ADMINISTRATOR = 'admin';
+exports.MANAGER = 'manager';
+exports.SELLER = 'seller';
+exports.SECRETARY = 'secretary';
+exports.MODERATOR = 'secretary';
+exports.CUSTOMER = 'customer';
+exports.SUPPLIER = 'supplier';
+exports.BASIC = 'basic';
+
+exports.hasRole = (item, role) => {
+  let { roles } = item;
+  if (roles === undefined) return false;
+  if (Array.isArray(role)) {
+    for(var i = 0; i<role.length; i++){
+      if (hasRole(item, role[i])) return true;
+    }
+    return false;
+  }
+  if (!Array.isArray(roles) || roles.length === 0 ) return false;
+  if (typeof roles[0] === 'object') roles = roles.map(v => v.id);
+  return ( roles && roles.includes(role) );
+}
